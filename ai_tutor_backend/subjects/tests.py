b3ed67cb7e502028cbec9a44a0021d4a1ae5440d
Devang Vangane <131960193+devangvangane@users.noreply.github.com>
2025-09-16 09:22:31 +0530

    Delete ai_tutor_backend directory

--- ai_tutor_backend/subjects/tests.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-from django.test import TestCase
-
-# Create your tests here.
-=======
-from django.test import TestCase
-
-# Create your tests here.
->>>>>>> 50b94bd00b5467019713ceacbdd16c2c3a09963a
